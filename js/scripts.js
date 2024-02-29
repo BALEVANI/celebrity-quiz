791675fa84f81c682b00094ac90a1c22c1a3f1aa
Adebayo Adeyeye Godwin <adebayoadeyeye889@gmail.com>
2024-02-29 09:31:16 +0100

    adding esle if in jagascript and script

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
         const question4 = $("input:radio[name = question4]:checked").val()
         const question5 = $("input:radio[name = question5]:checked").val()
 
-        if (question1 === "Black" && question3 === "Happy") {
+        if (question1 === "Black" && question2 === "Brown" && question3 === "Happy" && question4 === "Green" && question5 === "Not at all") {
             $("#main").hide()
             $("#result3").hide()
             $("#result2").hide()
